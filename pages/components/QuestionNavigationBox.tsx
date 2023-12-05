0421fd433fc7fa9e4a8ebb3590822aa98b3963d6
hovahyii <hovahyii@gmail.com>
2023-12-06 06:26:42 +0800

    add quiz

---
+++ pages/components/QuestionNavigationBox.tsx
@@ -0,0 +1,49 @@
+// components/QuestionNavigationBox.tsx
+import React from 'react';
+import styles from './QuestionNavigationBox.module.css';
+
+interface QuestionNavigationBoxProps {
+  totalQuestions: number;
+  currentQuestionIndex: number;
+  onQuestionClick: (index: number) => void;
+}
+
+const QuestionNavigationBox: React.FC<QuestionNavigationBoxProps> = ({
+  totalQuestions,
+  currentQuestionIndex,
+  onQuestionClick,
+}) => {
+  const mosCodeSection = Array.from({ length: 15 }, (_, index) => index);
+  const semaphoreCodeSection = Array.from({ length: 15 }, (_, index) => index + 15);
+
+  return (
+    <div className={styles.navigationBox}>
+      <div className={styles.section}>
+        <div className={styles.sectionTitle}>Morse Code</div>
+        {mosCodeSection.map((index) => (
+          <div
+            key={index + 1}
+            className={`${styles.questionNumber} ${index === currentQuestionIndex && styles.currentQuestion}`}
+            onClick={() => onQuestionClick(index)}
+          >
+            {index + 1}
+          </div>
+        ))}
+      </div>
+      <div className={styles.section}>
+        <div className={styles.sectionTitle}>Semaphore Code</div>
+        {semaphoreCodeSection.map((index) => (
+          <div
+            key={index + 1}
+            className={`${styles.questionNumber} ${index === currentQuestionIndex && styles.currentQuestion}`}
+            onClick={() => onQuestionClick(index)}
+          >
+            {index + 1}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionNavigationBox;
